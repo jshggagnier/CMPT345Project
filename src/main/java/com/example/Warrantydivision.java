519112a1e9af4da2a0a3dbc9772dd23b18aa8350
Owen Post <opost33@gmail.com>
2022-07-19 20:20:29 -0700

    added a warrantydivison object

---
+++ src/main/java/com/example/Warrantydivision.java
@@ -0,0 +1,24 @@
+package com.example;
+public class Warrantydivision 
+{
+    private String Brand;
+    private String EmailContact;
+
+    public String getBrand() {
+        return this.Brand;
+    }
+
+    public void setBrand(String Brand) {
+        this.Brand = Brand;
+    }
+
+    public String getEmailContact() {
+        return this.EmailContact;
+    }
+
+    public void setEmailContact(String EmailContact) {
+        this.EmailContact = EmailContact;
+    }
+
+
+}
